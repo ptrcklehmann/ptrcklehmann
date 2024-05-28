80f25ad29fe574c4ba04e80e13ea0adfe1cc22e5
Patrick Lehmann <ptrcklehmann@gmail.com>
2024-05-28 11:25:48 +0200

    chore: Optimize initial transition animation and update CSS styles

--- src/app/components/transitions/initial.tsx
+++ src/app/components/transitions/initial.tsx
@@ -5,40 +5,24 @@ import styles from "./transitions.module.css";
 import { ReactNode, useEffect, useRef, useState } from "react";
 
 const variants = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
-
-const rowVariants = {
-  open: {
-    y: 50,
+  open: (index: number) => ({
     opacity: 1,
-    transition: {
-      y: { stiffness: 1000, velocity: -100 },
-    },
-  },
+    transform: "translateY(0)",
+    transition: { ease: "linear", duration: 0.3, delay: index * 0.15 },
+  }),
   closed: {
-    y: 0,
     opacity: 0,
-    transition: {
-      y: { stiffness: 1000 },
-    },
+    transform: "translateY(-35px)",
   },
 };
 
 const Row = ({ children, index }: { children?: ReactNode; index: number }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, transform: "translateY(-35px)" }}
-      animate={{
-        opacity: 1,
-        transform: "translateY(0)",
-      }}
-      transition={{ ease: "linear", duration: 0.3, delay: index * 0.2 }}
+      custom={index}
+      initial="closed"
+      animate="open"
+      variants={variants}
       className={styles.row}
     >
       {children}
@@ -78,7 +62,7 @@ export const InitialTransition = () => {
                   <motion.h1
                     className={styles.title}
                     initial={{ opacity: 0, transform: "translateY(-35px)" }}
-                    exit={{ opacity: 0, transform: "translateY(35px)" }}
+                    exit={{ opacity: 0, transform: "translateY(-35px)" }}
                     animate={{ opacity: 1, transform: "translateY(0)" }}
                     transition={{ ease: "linear", duration: 0.2, delay: 0.3 }}
                   >
