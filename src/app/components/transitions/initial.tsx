67dc8e16985bc7e8fd58fb02eac71f8816e3beb1
Patrick Lehmann <ptrcklehmann@gmail.com>
2024-05-28 11:18:29 +0200

    chore: Update package dependencies, adjust CSS styles, and optimize initial transition

--- src/app/components/transitions/initial.tsx
+++ src/app/components/transitions/initial.tsx
@@ -1,8 +1,52 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useCycle } from "framer-motion";
 import styles from "./transitions.module.css";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const rowVariants = {
+  open: {
+    y: 50,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 0,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const Row = ({ children, index }: { children?: ReactNode; index: number }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, transform: "translateY(-35px)" }}
+      animate={{
+        opacity: 1,
+        transform: "translateY(0)",
+      }}
+      transition={{ ease: "linear", duration: 0.3, delay: index * 0.2 }}
+      className={styles.row}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const rows = [1, 2, 3, 4, 5];
 
 export const InitialTransition = () => {
   const [isFinishedLoading, setIsFinishedLoading] = useState(false);
@@ -11,8 +55,9 @@ export const InitialTransition = () => {
     const timer = setTimeout(() => {
       setIsFinishedLoading(true);
     }, 1500);
-
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
@@ -20,21 +65,28 @@ export const InitialTransition = () => {
       {!isFinishedLoading && (
         <>
           <motion.div
+            variants={variants}
             initial={{ height: 0 }}
             exit={{ height: 0 }}
             animate={{ height: "100%" }}
             transition={{ ease: "easeInOut", duration: 0.6 }}
             className={styles.initial}
           >
-            <motion.h1
-              className={styles.pageTitle}
-              initial={{ opacity: 0, transform: "translateY(-35px)" }}
-              exit={{ opacity: 0, transform: "translateY(35px)" }}
-              animate={{ opacity: 1, transform: "translateY(0)" }}
-              transition={{ ease: "linear", duration: 0.2, delay: 0.3 }}
-            >
-              Patrick Lehmann
-            </motion.h1>
+            {rows.map((row, index) => (
+              <Row key={row} index={index}>
+                {row === 3 ? (
+                  <motion.h1
+                    className={styles.title}
+                    initial={{ opacity: 0, transform: "translateY(-35px)" }}
+                    exit={{ opacity: 0, transform: "translateY(35px)" }}
+                    animate={{ opacity: 1, transform: "translateY(0)" }}
+                    transition={{ ease: "linear", duration: 0.2, delay: 0.3 }}
+                  >
+                    Patrick Lehmann
+                  </motion.h1>
+                ) : null}
+              </Row>
+            ))}
           </motion.div>
           <motion.div
             initial={{ height: "100%" }}
