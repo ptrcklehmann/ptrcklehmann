4f7a60dee82ed8921a14d4142e06d9c159ac97df
Patrick Lehmann <p.lehmann@code-b.com>
2024-12-28 21:19:18 +0100

    chore: Remove Profile component and its styled counterpart

--- src/app/components/profile/styled.ts
+++
@@ -1,8 +0,0 @@
-import { motion } from "framer-motion";
-import styled from "styled-components";
-
-export const Container = styled(motion.div)`
-  flex-grow: 1;
-  display: inline;
-  transform-origin: 0 0;
-`;
