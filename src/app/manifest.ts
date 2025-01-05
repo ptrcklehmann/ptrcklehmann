17f138c1c92f9091df723165584f6f706474f549
Patrick Lehmann <p.lehmann@code-b.com>
2025-01-05 12:10:03 +0100

    feat: Add manifest file for web app metadata configuration

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Patrick Lehmann',
+        short_name: 'Patrick Lehmann',
+        description:
+            'Patrick Lehmann, a creative front-end dev and your designer’s favorite programmer. With 7+ years crafting impactful products. Stay bold and say hi!',
+        start_url: '/',
+        display: 'fullscreen',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+        ],
+        lang: 'en',
+    };
+}
