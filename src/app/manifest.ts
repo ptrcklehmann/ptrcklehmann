afdb24804a63327e80d8cadb7e02a558797d8d9f
Patrick Lehmann <p.lehmann@code-b.com>
2025-03-23 10:35:42 +0100

    feat: Add new maskable icons to web app manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,6 +15,18 @@ export default function manifest(): MetadataRoute.Manifest {
                 sizes: 'any',
                 type: 'image/x-icon',
             },
+            {
+                src: '/web-app-manifest-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'maskable',
+            },
+            {
+                src: '/web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable',
+            },
         ],
         lang: 'en',
         theme_color: '#f4f6f5',
