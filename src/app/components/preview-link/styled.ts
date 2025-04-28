d02b74548882af2497caa4aed153bc72a8f7583a
Patrick Lehmann <ptrcklehmann.com>
2025-04-28 17:33:31 +0200

    feat: update HoverPreviewLink to use PreviewCard and enhance link data fetching

--- src/app/components/preview-link/styled.ts
+++ src/app/components/preview-link/styled.ts
@@ -36,9 +36,9 @@ export const LinkPreviewCard = styled(motion.span)`
     position: fixed;
     z-index: 9999;
     background-color: var(--bg-card);
-    padding: 0.6rem;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
-    border-radius: 6px;
+    padding: 1rem;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    border-radius: 10px;
     max-width: 300px;
     display: flex;
     flex-direction: column;
