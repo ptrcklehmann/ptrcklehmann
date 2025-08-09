e6da292cebc502ea1047eb3c455ec1d520df0dda
patrick lehmann <ptrcklehmann@gmail.com>
2025-08-09 09:34:16 +0200

    refactor: simplify day name logic

--- src/app/components/main/animated-message.tsx
+++ src/app/components/main/animated-message.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { AnimatedMessageContainer, DesktopHiddenBr } from './styled';
 
@@ -21,7 +20,7 @@ const scrambleTexts = adjectives.flatMap(item => [item, 1800]);
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export const AnimatedMessage = () => {
-    const dayname = useMemo(() => weekdays[new Date().getDay()], []);
+    const dayname = weekdays[new Date().getDay()];
 
     return (
         <AnimatedMessageContainer>
